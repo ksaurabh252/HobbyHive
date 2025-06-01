7789ed48ff59f89ad9e14db9f6d484d82d22f1a5
ksaurabh252 <ksaurabh252@gmail.com>
2025-06-01 16:29:46 +0530

    accept both auth headers (x-auth-token and Authorization)

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  // Get token from header
-  const token = req.header("x-auth-token");
+  // Check both headers
+  const token =
+    req.header("x-auth-token") ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
-  // Check if no token
   if (!token) {
     return res.status(401).json({ error: "No token, authorization denied" });
   }
 
-  // Verify token
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
